ee283875c4a7f684e6f3cb26e70ab67de92e513a
Yukkitorie <manatormmillhaus@gmail.com>
2025-07-10 13:15:39 +0100

    help me please

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="task-container">
+        <h1>Task List With Priority</h1>
+        <button className="close-button">+</button>
+      </div>
+
+      <div className="task-container">
+        <h2>Tasks</h2>
+        <button className="close-button">+</button>
+        <TaskForm />
+      </div>
+
+      <div className="completed-task-container">
+        <h2>Completed Tasks</h2>
+        <button className="close-button">+</button>
+      </div>
+    </div>
+  );
+}
+
+function TaskForm() {
+  return (
+    <form action="" className="task-form">
+      <input type="text" value={""} placeholder="task title" required />
+      <select value={""}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <input type="datetime-local" required value={""} />
+      <button type="submit">Add task</button>
+    </form>
+  );
 }
 
 export default App;
